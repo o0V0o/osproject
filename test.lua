2961dca79230b9d2fa8b6e34c52c9af6fe72d76a
Alex <o0o.moro.o0o@gmail.com>
2016-03-06 23:01:01 -0600

    added placementAlgorithms

--- test.lua
+++ test.lua
@@ -7,11 +7,13 @@ local RNG = require('rng')
 local sim = Simulation()
 sim.placementAlgorithm = algorithms.bestFit
 sim:reset()
+--do a test of the rng
 for i=1,10000 do
 	sim.logs.rngtest[i] = RNG(0,100,10)
 end
 sim:report('rngtest.csv')
 
+--and now step through the memory configurations
 local lastMemConfig = ''
 return function()
 	while true do
